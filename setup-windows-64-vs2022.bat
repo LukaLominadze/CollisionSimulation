a8a2b5a31112d90ce2b629c82875c745198bda97
MustachoMan <fguru974@gmail.com>
2024-10-11 21:25:53 +0400

    updating build files

--- setup-windows-64-vs2022.bat
+++ setup-windows-64-vs2022.bat
@@ -1,7 +1,5 @@
 git submodule add https://github.com/LukaLominadze/GLRenderer Submodules
 
-vendor\bin\scripts\Build-submodules.py
-
 Submodules\vendor\bin\scripts\premake\premake5.exe vs2022 premake5.lua
 
 vendor\bin\scripts\Setup-win-64-vs2022.py
